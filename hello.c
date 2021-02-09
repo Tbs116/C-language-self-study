830ce68639bdacaa45a338458a2a1f43cca9153a
SRK <63025838+Tbs116@users.noreply.github.com>
2021-02-09 16:33:38 +0800

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	printf("hello,word\n");
+	printf("are you ok\n");
+		sleep(1);
+	printf("ok?桌面\n");
+	return 0;
+}
+
+
+
+
+
